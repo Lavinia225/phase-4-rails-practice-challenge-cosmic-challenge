e949a82d48a79571d39065f71e58535f351e4a7e
Luna Seifert <thepuddingdeity@gmail.com>
2023-06-18 13:42:01 -0500

    commit tree e865bf01fc988f9c5e62e8a1c96cff185e3a05f6

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,4 +7,21 @@ class PlanetsController < ApplicationController
 
     render json: planets
   end
+
+  def show
+    planet = PLanet.find(params[:id])
+    render json: planet
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
